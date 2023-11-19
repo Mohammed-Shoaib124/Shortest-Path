98c8e737b1ecbe6c11bf30a6da8a46b57275bf5c
samhithakasim <151407257+samhithakasim@users.noreply.github.com>
2023-11-18 18:49:32 -0600

    Add files via upload

---
+++ CityData.java
@@ -0,0 +1,27 @@
+//package cities;
+//import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.ArrayList; 
+
+
+public class CityData {
+    public int temperature;
+    public List<Integer> distanceData;
+
+    public CityData(int temperature) {
+        this.temperature = temperature;
+        this.distanceData = new ArrayList<>();
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public List<Integer> getDistanceData() {
+        return distanceData;
+    }
+
+    public void setDistanceData(List<Integer> distanceData) {
+        this.distanceData = distanceData;
+    }
+}
